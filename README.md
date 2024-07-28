4ffb71476186350886221af02961b5a79a4ccdf2
Alisson Zampietro <alisson.zampietro@gmail.com>
2024-07-28 21:18:01 +0100

    chore: adding docs and resources

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # EzStock
 
-![Architecture](/assets/ezstock.png)
+![Architecture](/assets/images/ezstock.png)
+
+## How to run the project
+
+ - Rename .env.examples variables (one in the root and the other one into ezstock folder)
+   - The root .env is related to docker.compose environment
+
+
+## Import postman
+You can find all postman files inside the folder `assets/postman`. Just import both files to your postman app.
---
+++ ezstock/migrations/Version20240728195638.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240728195638 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE stock_history (id INT AUTO_INCREMENT NOT NULL, user_id_id INT NOT NULL, symbol VARCHAR(15) NOT NULL, open DOUBLE PRECISION NOT NULL, high DOUBLE PRECISION NOT NULL, low DOUBLE PRECISION NOT NULL, close DOUBLE PRECISION NOT NULL, datetime DATETIME NOT NULL, INDEX IDX_3E1C60E89D86650F (user_id_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, roles JSON DEFAULT \'["ROLE_USER\' NOT NULL COMMENT \'(DC2Type:json)\', UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE stock_history ADD CONSTRAINT FK_3E1C60E89D86650F FOREIGN KEY (user_id_id) REFERENCES user (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE stock_history DROP FOREIGN KEY FK_3E1C60E89D86650F');
+        $this->addSql('DROP TABLE stock_history');
+        $this->addSql('DROP TABLE user');
+    }
+}
